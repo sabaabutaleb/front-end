654ae678f96006b229e5d5d86429911ef9a2d48b
saba <saba.abutaleb@hotmail.com>
2024-03-28 19:46:39 -0500

    html & Css practice

---
+++ Iphone project/main.js
@@ -0,0 +1,8 @@
+let image=document.querySelector(".centered-image");
+function getPhone(src){
+  image.src=src;
+}
+let backgroundColor=document.querySelector(".container");
+function changeBackground(newColor){
+  backgroundColor.style.background=newColor;
+}
